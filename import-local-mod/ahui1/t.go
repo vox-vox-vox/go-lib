6e9c158c8a564f0c7a406d16ca6a334f861dbe05
Xuehui Ye <yexuehui@momenta.ai>
2019-03-25 14:34:10 +0800

    init

--- import-local-mod/ahui1/t.go
+++ import-local-mod/ahui1/t.go
@@ -6,6 +6,9 @@ var now = time.Now()
 func init() {
     fmt.Printf("now: %v\n", now)
 }
+func init() {
+    fmt.Printf("since: %v\n", time.Now().Sub(now))
+}
 
 func Test3(){
     println("test3 by Ahuix")
