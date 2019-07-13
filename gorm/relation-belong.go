1a2b066e11ed434c931ce527eb124a1675d66fd7
Xuehui Ye <yexuehui@momenta.ai>
2019-07-13 11:30:14 +0800

    belong

--- gorm/relation-belong.go
+++ gorm/relation-belong.go
@@ -20,23 +20,13 @@ type Profile struct {
 
 
 func main() {
-    db, err := gorm.Open("postgres", "host=localhost user=postgres dbname=ahuigo sslmode=disable password=")
-    db.LogMode(true)
-    if err != nil {
-        panic("failed to connect database")
-    }
-    t1 := time.Now()
-    u := User{ID: 1}
-    profile := Profile{ID: 1}
-    db.Model(&u).Related(&profile)
-    //db.Model(&u).Find(&u)
-    //db.Model(&u).Related(&u.CreditCard,"CreditCard")
-    //db.Model(&u).Related(&u.Emails,"Emails")
-    //db.Model(&u).Related(&u.Languages, "Languages")
-    //db.Model(&u).Related(&u.BillingAddress,"BillingAddress")
-    //db.Model(&u).Related(&u.ShippingAddress,"ShippingAddress")
-    fmt.Println(u)
-    t2 := time.Now()
-    fmt.Println("gorm耗时:", t2.Sub(t1))
+    type A struct{int}
+    a:=A{}
+    b:=A{}
+    fmt.Printf("%p,%p,%v\n", &a,&b, a.int)  //not same
+    b = a
+    fmt.Printf("%p,%p\n", &a,&b)  //not same
+
+time.Now()
 }
 
