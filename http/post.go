f1cf011fbe9bb5767b4bc300eeb55a21a498e1cd
ahuigo <ahuigo@ahuigo.com>
2020-03-31 16:45:54 +0800

    session

--- http/post.go
+++ http/post.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
     "net/url"
+    "strings"
 	"net/http"
     "io/ioutil"
 	_ "log"
@@ -12,8 +13,9 @@ import (
 
 func main() {
     //resp, _ := http.PostForm("https://httpbin.org/cookies/set?ahui=mo",
-    resp, _ := http.PostForm("https://httpbin.org/post",
-	url.Values{"key": {"Value"}, "id[]": {"123"}})
+    resp, _ := http.PostForm("https://httpbin.org/post", url.Values{"key": {"Value"}, "id": {"123"}})
+    //resp, _ := http.PostForm("https://httpbin.org/post", strings.NewReader("username=admin&password=test"))
+    resp, _ = http.Post("http://127.0.0.1:5000/header.php", "application/x-www-form-urlencoded", strings.NewReader("username=admin&password=pass"))
     pn := fmt.Println
     body, _:= ioutil.ReadAll(resp.Body)
     pn(string(body))
---
+++ http/session.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+    _ "net/url"
+	"net/http"
+    "io/ioutil"
+	_ "log"
+	_ "reflect"
+	_ "bytes"
+    "net/http/cookiejar"
+)
+
+func main() {
+    //resp, _ := http.PostForm("https://httpbin.org/cookies/set?ahui=mo",
+    cookieJar, _ := cookiejar.New(nil)
+
+    client := &http.Client{
+        Jar: cookieJar,
+        CheckRedirect: func(req *http.Request, via []*http.Request) error {
+            return http.ErrUseLastResponse
+        },
+    }
+
+    resp, _ := client.Get("http://127.0.0.1:5000/header.php")
+    resp, _ = client.Get("http://bing.com")
+    //ioutil.ReadAll(resp.Body)
+    resp, _ = client.Get("http://127.0.0.1:5000/header.php")
+    for k, v := range resp.Header{
+		fmt.Printf("k=%v, v=%v\n", k, v)
+	}
+    for i, cookie := range resp.Cookies() {
+        fmt.Println("Found a cookie:",i, cookie.Name,"=", cookie.Value)
+    }
+    pn := fmt.Println
+    body, _:= ioutil.ReadAll(resp.Body)
+    pn(string(body))
+    location, _ :=resp.Location()
+    fmt.Println(location)
+}
