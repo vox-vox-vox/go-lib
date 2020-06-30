f892c6d3d12e33956d7da2d7af15610b74b830ee
ahuigo <ahuigo@ahuigo.com>
2020-06-30 15:41:39 +0800

    shell

---
+++ import-local-mod/ahui1/t2.go
@@ -0,0 +1,14 @@
+/*
+Package ahui1 这是一个本地包
+*/
+package ahui1
+//import "time"
+import "fmt"
+
+func init() {
+    fmt.Printf("t2.go: exec init1\n", )
+}
+func init() {
+    fmt.Printf("t2.go: exec init2\n", )
+}
+
