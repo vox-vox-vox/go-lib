78549329ee240676faefb517007107172e74bf6c
ahuigo <ahuigo@ahuigo.com>
2020-10-31 18:45:36 +0800

    enum

---
+++ enum/enum.go
@@ -0,0 +1,9 @@
+package main
+type PolicyType int32
+
+const (
+    Policy_MIN      PolicyType = 0
+    Policy_MAX      PolicyType = 1
+    Policy_MID      PolicyType = 2
+    Policy_AVG      PolicyType = 3
+)
