316b4f46a58b14d223d7270338732ccd523f5f81
ahuigo <ahuigo@ahuigo.com>
2021-04-22 00:08:34 +0800

    interface

--- object/interface.go
+++ object/interface.go
@@ -7,4 +7,7 @@ import (
 func main() {
     m:= map[string]interface{}{"a":1}
     fmt.Printf("a=%#T\n", m["a"].(int))
+    fmt.Printf("a=%#T\n", m["a"])
+    b:= map[string]interface{}{"b":[]string{"str1"}}
+    fmt.Printf("b=%#T\n", b["b"])
 }
