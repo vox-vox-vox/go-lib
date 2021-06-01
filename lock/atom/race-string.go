2df69975f078747132961f206bd6ace82020d67c
ahuigo <ahuigo@ahuigo.com>
2021-06-01 23:00:45 +0800

    example race string

--- lock/atom/race-string.go
+++ lock/atom/race-string.go
@@ -7,12 +7,12 @@ import (
 // 交替打印字符
 // go-lib/goroutine/race-string.go
 const (
-  FIRST  = "WHAT THE"
-  SECOND = "F*CK"
+  FIRST  = "WHAT THE Fuck"
+  SECOND = "F*CK 123456 89 abcdef"
 )
 
 func main() {
-  var s string
+  var s string = FIRST
   go func() {
     i := 1
     for {
@@ -22,12 +22,22 @@ func main() {
       } else {
         s = SECOND
       }
-      time.Sleep(10)
+     time.Sleep(10)
+      //time.Sleep(10*time.Second)
     }
   }()
 
   for {
-    fmt.Println(s)
+      if (s==FIRST || s==SECOND){
+        fmt.Println(s)
+          fmt.Printf("first string:%v\n",s==FIRST)
+          fmt.Printf("second string:%v\n",s==SECOND)
+      }else{
+          fmt.Printf("---------------\nlast string:%v\n",s)
+          fmt.Printf("first string:%v\n",s==FIRST)
+          fmt.Printf("second string:%v\n",s==SECOND)
+          panic("err string")
+      }
     time.Sleep(10)
   }
 }
