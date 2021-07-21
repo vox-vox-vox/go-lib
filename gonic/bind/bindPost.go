698be705da68c9104c4eb7539dc4c6567c82f710
ahuigo <ahuigo@ahuigo.com>
2021-07-21 22:38:54 +0800

    bind

--- gonic/bind/bindPost.go
+++ gonic/bind/bindPost.go
@@ -9,16 +9,22 @@ type UserReq struct {
 	Password string `gorm:"not null" json:"password" form:"password"`
 }
 
+/**
+nc -l 8088
+curl  'm:8088' -F 'password=ahui1'
+curl  -X POST -H 'Content-Type: application/x-www-form-urlencoded' 'm:8088' -d 'password=ahui1'
+
+ */
 func main() {
     router := gin.Default()
 
     router.POST("/", func(c *gin.Context) {
         req := UserReq{}
-        if err := c.ShouldBind(req); err != nil {
+        if err := c.ShouldBind(&req); err != nil {
             c.AbortWithError(http.StatusBadRequest, err)
             return
         }
-        c.JSON(http.StatusOK, "succ")
+        c.JSON(http.StatusOK, req)
     })
 
     router.Run(":8088")
